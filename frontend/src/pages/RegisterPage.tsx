e70d195419f41d93b406c5c47b4c096b181b71bc
lukaliangjun <ljunchen@foxmail.com>
2026-07-09 16:53:31 +0800

    feat(frontend): mount Footer on RegisterPage with absolute bottom positioning

--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -4,6 +4,7 @@ import { useRegisterMutation, useSendCodeMutation, useRegisterByEmailMutation } 
 import { useAuthContext } from '../contexts/AuthContext'
 import { features } from '../config/features'
 import EyeIcon from '../components/EyeIcon'
+import Footer from '../components/Footer'
 
 type RegisterTab = 'username' | 'email'
 
@@ -504,6 +505,9 @@ function RegisterPage() {
             </p>
           </div>
         </div>
+      </div>
+      <div className="absolute bottom-0 left-0 right-0 z-10">
+        <Footer />
       </div>
     </div>
   )
